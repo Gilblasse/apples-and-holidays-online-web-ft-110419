a3e90908f8202fa774a137da44102e0b2a5ce025
Nethelbert <gilblasse@gmail.com>
2019-11-07 19:01:49 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,33 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash[:winter].values.each {|array| array << supply}
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+  holiday_hash[:spring][:memorial_day] << supply
 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash[season][holiday_name] = supply_array
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[holiday_hash.keys.find{|k| k == :winter}].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,13 +55,35 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  
+  holiday_hash.each do |main_hash,value_hash| 
+		puts "#{main_hash.to_s.capitalize}:"
+		value_hash.map do|k,v|
+			if k.to_s.split('_')[1].nil?
+				title = k.to_s.capitalize
+			else
+				title = (k.to_s.split('_').map{|w| w.capitalize}).join(' ')
+			end
+			supplies = v.join(', ')
+			puts "  #{title}: #{supplies}"
+		end
+	end
 
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  my_key = []
+		holiday_hash.each do |k,v_hash| 
+			myarra = v_hash.each do|k,v|
+				if v.include?("BBQ")
+				 my_key << k
+				end
+			end
+			
+		end
+		my_key
 end
 
 
